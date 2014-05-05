4e2731d6d4f9f8e849ef55240d909abf4a0e6fe4
Gabriel de Tassigny <gabriel@multivac.cc>
2014-05-05 22:57:28 +0200

    Can export one or more pages

--- scripts/export.jsx
+++ scripts/export.jsx
@@ -1,25 +1,11 @@
-function export_pages(doc, path) {
-  var name = doc.name;
-  for (var i = 0; i < doc.pages.length; i++) {
-    page = doc.pages.item(i);
-    app.JPEGExportPreference.exportResolution = quality;
-  }
-}
-
-function export_elem() {
-  if (format == 'jpeg')
-  {
-    app.JPEGExportPreference.exportResolution = quality;
-    //app.JPEGExportPreference.exportingSpread = true;
-  }
-}
-
 var path = '/Users/detass_g/Etna/EIP/InDesign-Scripts/'; // TODO: load it dynamically
 
 var doc_name = app.scriptArgs.getValue('document');
 var format = app.scriptArgs.getValue('format');
 var size = app.scriptArgs.getValue('size');
 var quality = app.scriptArgs.getValue('quality');
+var page_range = app.scriptArgs.getValue('pages');
+var spreads = app.scriptArgs.getValue('spreads');
 
 var message = 'OK';
 
@@ -28,7 +14,7 @@ if (format.length == 0)
 if (size.length == 0)
   size = '100';
 if (quality.length == 0)
-  quality = '72';
+  quality = '100';
 if (doc_name.length == 0)
   message = 'Error : No document parameter specified!';
 
@@ -40,11 +26,23 @@ var export_format = null;
 if (format == 'png') {
   export_format = ExportFormat.PNG_FORMAT;
   app.pngExportPreferences.exportResolution = Number(quality);
-} else if (format == 'jpeg') {
+  if (page_range.length != 0) {
+    app.pngExportPreferences.pngExportRange = PNGExportRangeEnum.EXPORT_RANGE;
+    app.pngExportPreferences.pageString = page_range;
+  }
+  else
+    app.pngExportPreferences.pngExportRange = PNGExportRangeEnum.EXPORT_ALL;
+} 
+else if (format == 'jpeg') {
   export_format = ExportFormat.JPG;
   app.jpegExportPreferences.exportResolution = Number(quality);
+  if (page_range.length != 0) {
+    app.jpegExportPreferences.jpegExportRange = ExportRangeOrAllPages.EXPORT_RANGE;
+    app.jpegExportPreferences.pageString = page_range;
+  }
+  else
+    app.jpegExportPreferences.jpegExportRange = ExportRangeOrAllPages.EXPORT_ALL;
 }
 doc.exportFile(export_format, img_file);
-
 doc.close();
 message;
