f07165eab825ed28bbe2618b32341d940f6cb6c4
Gabriel de Tassigny <gabriel@multivac.cc>
2014-05-07 21:24:09 +0200

    Put prameters parsing in a separated function

--- scripts/export.jsx
+++ scripts/export.jsx
@@ -1,48 +1,59 @@
-var path = '/Users/detass_g/Etna/EIP/InDesign-Scripts/'; // TODO: load it dynamically
-
-var doc_name = app.scriptArgs.getValue('document');
-var format = app.scriptArgs.getValue('format');
-var size = app.scriptArgs.getValue('size');
-var quality = app.scriptArgs.getValue('quality');
-var page_range = app.scriptArgs.getValue('pages');
-var spreads = app.scriptArgs.getValue('spreads');
+function load_parameters(args, default_values) {
+  var hash = {};
+  for (var i = 0; i < args.length; i++) {
+    hash[args[i]] = app.scriptArgs.getValue(args[i]);
+    if (hash[args[i]].length == 0) {
+      if (args[i] in default_values)
+        hash[args[i]] = default_values[args[i]];
+      else {
+        message = 'Error : undefined required parameter : ' + args[i];
+        return null;
+      }
+    }
+  }
+  return hash;
+}
 
 var message = 'OK';
+var config = load_parameters(['document', 'format', 'size', 'quality', 'pages', 'spreads'], 
+  {
+    format: 'jpeg',
+    size: '100',
+    quality: '100',
+    pages: null,
+    spreads: null
+  });
 
-if (format.length == 0)
-  format = 'jpeg';
-if (size.length == 0)
-  size = '100';
-if (quality.length == 0)
-  quality = '100';
-if (doc_name.length == 0)
-  message = 'Error : No document parameter specified!';
+if (config != null) {
+  var path = '/Users/detass_g/Etna/EIP/InDesign-Scripts/';
+  var file = new File(path + 'documents/' + config['document'] + '.indd');
+  var img_file = new File(path + 'results/' + config['document'] + '.' + config['format']);
 
-var file = new File(path + 'documents/' + doc_name + '.indd');
-var img_file = new File(path + 'results/' + doc_name + '.' + format);
-
-var doc = app.open(file);
-var export_format = null;
-if (format == 'png') {
-  export_format = ExportFormat.PNG_FORMAT;
-  app.pngExportPreferences.exportResolution = Number(quality);
-  if (page_range.length != 0) {
-    app.pngExportPreferences.pngExportRange = PNGExportRangeEnum.EXPORT_RANGE;
-    app.pngExportPreferences.pageString = page_range;
+  var doc = app.open(file);
+  var export_format = null;
+  if (config['format'] == 'png') {
+    export_format = ExportFormat.PNG_FORMAT;
+    app.pngExportPreferences.exportResolution = Number(config['quality']);
+    if (config['pages']!= null) {
+      app.pngExportPreferences.pngExportRange = PNGExportRangeEnum.EXPORT_RANGE;
+      app.pngExportPreferences.pageString = config['pages'];
+    }
+    else
+      app.pngExportPreferences.pngExportRange = PNGExportRangeEnum.EXPORT_ALL;
+  } 
+  else if (config['format'] == 'jpeg') {
+    export_format = ExportFormat.JPG;
+    app.jpegExportPreferences.exportResolution = Number(config['quality']);
+    if (config['pages'] != null) {
+      app.jpegExportPreferences.jpegExportRange = ExportRangeOrAllPages.EXPORT_RANGE;
+      app.jpegExportPreferences.pageString = config['pages'];
+    }
+    else
+      app.jpegExportPreferences.jpegExportRange = ExportRangeOrAllPages.EXPORT_ALL;
   }
-  else
-    app.pngExportPreferences.pngExportRange = PNGExportRangeEnum.EXPORT_ALL;
-} 
-else if (format == 'jpeg') {
-  export_format = ExportFormat.JPG;
-  app.jpegExportPreferences.exportResolution = Number(quality);
-  if (page_range.length != 0) {
-    app.jpegExportPreferences.jpegExportRange = ExportRangeOrAllPages.EXPORT_RANGE;
-    app.jpegExportPreferences.pageString = page_range;
-  }
-  else
-    app.jpegExportPreferences.jpegExportRange = ExportRangeOrAllPages.EXPORT_ALL;
+
+  doc.exportFile(export_format, img_file);
+  doc.close();
 }
-doc.exportFile(export_format, img_file);
-doc.close();
+
 message;
